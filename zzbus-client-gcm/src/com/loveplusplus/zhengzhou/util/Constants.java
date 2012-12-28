96e83ee22f8f0cc622ab0a4c7224f333eb7fd798
feicien <ithcheng@gmail.com>
2012-12-28 18:36:02 +0800

    Revert "Revert "test""
    This reverts commit a1c09fc01d29ab5f9b38b2d2a9746778db8a3ce5.

--- zzbus-client-gcm/src/com/loveplusplus/zhengzhou/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.loveplusplus.zhengzhou.util;
-
-public class Constants {
-
-	public static final String CHARSET_NAME = "UTF-8";
-	public static final String SHARED_PREFERENCE_NAME = null;
-	public static final String NOTIFICATION_ICON = null;
-
-}
